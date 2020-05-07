776dfb82c90a8cfd722f4b62ca8d1cfeef513640
cassiofb-dev <cassiofb.souza@gmail.com>
2020-05-07 17:38:20 -0300

    Updates

---
+++ precache-manifest.1f9662eb8c0b2294ae0f8d1a1e18bf55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f509f8ce4eea8e2f9e75ab6a2739a88",
+    "url": "/OmniStack11/index.html"
+  },
+  {
+    "revision": "9f3ad1230e37f4c0748f",
+    "url": "/OmniStack11/static/css/main.8f8c8aff.chunk.css"
+  },
+  {
+    "revision": "803a7fc64111547b7405",
+    "url": "/OmniStack11/static/js/2.b711ed42.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/OmniStack11/static/js/2.b711ed42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3ad1230e37f4c0748f",
+    "url": "/OmniStack11/static/js/main.29a1681a.chunk.js"
+  },
+  {
+    "revision": "2db90c56163e2092d07c",
+    "url": "/OmniStack11/static/js/runtime-main.da372ac9.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/OmniStack11/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/OmniStack11/static/media/logo.7eea718b.svg"
+  }
+]);
